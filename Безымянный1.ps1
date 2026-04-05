bf6e644ed64d70671d6e3f4b719af7a8ee7faecf
begula1125-stack <begula1125@gmail.com>
2026-04-05 10:20:54 +0500

    Add files via upload

---
+++ Безымянный1.ps1
@@ -0,0 +1,16 @@
+﻿$source = "C:\Users\pipidastr"
+$destination = "C:\Users\pipidastr\Documents\html_pidoras\pipidastrgames"
+
+# Список того, что нужно забрать
+$items = @("css", "js", "clickereshkere", "index.html")
+
+foreach ($item in $items) {
+    $itemPath = Join-Path $source $item
+    if (Test-Path $itemPath) {
+        Move-Item -Path $itemPath -Destination $destination -Force
+        Write-Host "✅ Перемещено: $item" -ForegroundColor Green
+    }
+}
+
+Set-Location $destination
+Write-Host "🚀 Теперь ты в нужной папке: $destination" -ForegroundColor Cyan
